e9a86a5013dad2dc6072edb3321f24e58040dc0d
kubilaysabah <kubilaysabah@gmail.com>
2025-08-20 11:09:29 +0300

    update button story

--- src/components/button/button.stories.svelte
+++ src/components/button/button.stories.svelte
@@ -1,32 +1,29 @@
 <script module lang="ts">
-	import { defineMeta, type StoryContext } from '@storybook/addon-svelte-csf';
-	import { type ComponentProps } from 'svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
 
 	import Button from './button.svelte';
 
 	const { Story } = defineMeta({
 		component: Button
 	});
-
-	type Args = ComponentProps<typeof Button>;
 </script>
 
-<Story name="Small" args={{ variant: "fill", color: "primary", size: "small" }}>
+<Story name="Small" args={{ variant: 'fill', color: 'primary', size: 'small' }}>
 	{#snippet children()}Small{/snippet}
 </Story>
 
-<Story name="Medium" args={{ variant: "fill", color: "primary", size: "medium" }}>
+<Story name="Medium" args={{ variant: 'fill', color: 'primary', size: 'medium' }}>
 	{#snippet children()}Medium{/snippet}
 </Story>
 
-<Story name="Large" args={{ variant: "fill", color: "primary", size: "large" }}>
+<Story name="Large" args={{ variant: 'fill', color: 'primary', size: 'large' }}>
 	{#snippet children()}Large{/snippet}
 </Story>
 
-<Story name="Outline" args={{ variant: "outline", color: "primary", size: "small" }}>
+<Story name="Outline" args={{ variant: 'outline', color: 'primary', size: 'small' }}>
 	{#snippet children()}Outline{/snippet}
 </Story>
 
-<Story name="Ghost" args={{ variant: "ghost", color: "primary", size: "small" }}>
+<Story name="Ghost" args={{ variant: 'ghost', color: 'primary', size: 'small' }}>
 	{#snippet children()}Ghost{/snippet}
 </Story>
