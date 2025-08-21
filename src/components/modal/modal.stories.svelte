8789aac249c19f1e1267a66a0ee418a3105556de
kubilaysabah <kubilaysabah@gmail.com>
2025-08-21 11:13:39 +0300

    update modal

--- src/components/modal/modal.stories.svelte
+++ src/components/modal/modal.stories.svelte
@@ -1,6 +1,6 @@
 <script module lang="ts">
 	import { defineMeta } from '@storybook/addon-svelte-csf';
-    import { Button } from '$components';
+	import { Button } from '$components';
 	import Modal from './modal.svelte';
 
 	const { Story } = defineMeta({
@@ -8,38 +8,43 @@
 		title: 'Components/Modal',
 		tags: ['autodocs'],
 		argTypes: {
-            closeOnBackdropClick: {
-                control: 'boolean',
-                description: 'Whether the modal should close when the backdrop is clicked'
-            },
-            closeOnEscape: {
-                control: 'boolean',
-                description: 'Whether the modal should close when the escape key is pressed'
-            },
-            forceClose: {
-                control: 'boolean',
-                description: 'Whether the modal should be forcefully closed'
-            },
-            size: {
-                control: 'select',
-                options: ['sm', 'md', 'lg', 'xl', 'xxl', 'fit'],
-                description: 'Size of the modal'
-            },
-            title: {
-                control: 'text',
-                description: 'Title of the modal'
-            }
+			closeOnBackdropClick: {
+				control: 'boolean',
+				description: 'Whether the modal should close when the backdrop is clicked'
+			},
+			closeOnEscape: {
+				control: 'boolean',
+				description: 'Whether the modal should close when the escape key is pressed'
+			},
+			forceClose: {
+				control: 'boolean',
+				description: 'Whether the modal should be forcefully closed'
+			},
+			size: {
+				control: 'select',
+				options: ['sm', 'md', 'lg', 'xl', 'xxl', 'fit'],
+				description: 'Size of the modal'
+			},
+			title: {
+				control: 'text',
+				description: 'Title of the modal'
+			}
 		}
 	});
 </script>
 
-
-<Story name="Default Usage" args={{
-    closeOnBackdropClick: true,
-    closeOnEscape: true,
-    size: 'md',
-    title: 'Default Modal'
-}}>
-    <Button size="small" variant="outline" color="primary">Close</Button>
-    <Button size="small" variant="fill" color="primary">Submit</Button>
+<Story
+	name="Default Usage"
+	args={{
+		closeOnBackdropClick: true,
+		closeOnEscape: true,
+		size: 'md',
+		title: 'Default Modal'
+	}}
+>
+	<p class="text-lg">{'This is a default modal'}</p>
+	<nav class="flex flex-wrap items-center justify-end space-x-2">
+		<Button size="small" variant="outline" color="primary">Close</Button>
+		<Button size="small" variant="fill" color="primary">Submit</Button>
+	</nav>
 </Story>
